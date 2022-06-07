686656411e0c238faf27932df93ab7a6e7c99428
Ilesh Thiada <ileshkt@gmail.com>
2022-06-07 22:28:00 +0530

    Update and rename setup.sh to setup.bash

--- setup.bash
+++ setup.bash
@@ -1,6 +1,6 @@
 curl -sSf https://sh.rustup.rs | sh -s -- -y
 
-if [[ $OSTYPE == *"ubuntu"* ]]; then
+if [[ $(cat /etc/os-release) == *"ubuntu"* ]]; then
     curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
     sudo apt update
@@ -13,7 +13,7 @@ if [[ $OSTYPE == *"darwin"* ]]; then
     brew upgrade
 fi
 
-if [[ $OSTYPE == *"ubuntu"* ]]; then
+if [[ $(cat /etc/os-release) == *"ubuntu"* ]]; then
     sudo apt install openjdk-18-jdk fish aria2 exa httpie just neovim bat gh
 fi
 if [[ $OSTYPE == *"darwin"* ]]; then
