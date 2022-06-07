e00feb52404cccc06caab85457f441d401c8706d
Ilesh Thiada <ileshkt@gmail.com>
2022-06-07 22:02:03 +0530

    Intial commit

---
+++ setup.sh
@@ -0,0 +1,23 @@
+curl -sSf https://sh.rustup.rs | sh -s -- -y
+
+if [[ $OSTYPE == *"ubuntu"* ]]; then
+    curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+    sudo apt update
+    sudo apt upgrade -y
+fi
+if [[ $OSTYPE == *"darwin"* ]]; then
+    export NONINTERACTIVE=1
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    brew update
+    brew upgrade
+fi
+
+if [[ $OSTYPE == *"ubuntu"* ]]; then
+    sudo apt install openjdk-18-jdk fish aria2 exa httpie just neovim bat gh
+fi
+if [[ $OSTYPE == *"darwin"* ]]; then
+    brew install openjdk fish aria2 exa httpie just neovim bat gh
+fi
+
+chsh -s $(which fish)
